eca89072f36a03204dbf3ffe504b9e4b2da98129
yeezy src <dustinmathews2000@outlook.de>
2025-04-05 02:02:01 +0200

    Update tool3.py

--- tool3.py
+++ tool3.py
@@ -1,44 +1,22 @@
 import random
-import time
-from colorama import Fore, Back, Style, init
 import sys
+from colorama import Fore
 
-# Initialisiert colorama
-init(autoreset=True)
-
-def generate_random_binary_stream():
-    """Generiert einen endlosen Strom von bunten 1en und 0en"""
+def turbo_binary_rain():
+    """Absolut schnellste Implementierung"""
+    colors = [Fore.GREEN, Fore.LIGHTGREEN_EX]
+    sys.stdout.write('\033[?25l')  # Cursor verstecken
+    
     try:
         while True:
-            # Zufällige Länge für den nächsten Block (zwischen 10 und 50 Zeichen)
-            block_length = random.randint(10, 50)
+            # Mega-Block mit 5000 Zeichen
+            bits = ''.join(random.choices('01', k=5000))
+            # Sofortige Ausgabe
+            sys.stdout.write(''.join(random.choice(colors) + b for b in bits))
+            sys.stdout.flush()
             
-            for _ in range(block_length):
-                bit = random.choice(['0', '1'])
-                
-                # Wähle zufällig zwischen grün und hellgrün
-                color = random.choice([Fore.GREEN, Fore.LIGHTGREEN_EX])
-                
-                # Schreibe das Zeichen ohne Newline
-                sys.stdout.write(color + bit)
-                sys.stdout.flush()
-                
-                # Kurze Pause für den Matrix-Effekt
-                time.sleep(0.05)
-            
-            # Neue Zeile nach jedem Block mit 50% Wahrscheinlichkeit
-            if random.random() > 0.5:
-                print()
-                
     except KeyboardInterrupt:
-        print("\n\n" + Fore.RED + "Stream gestoppt")
+        sys.stdout.write('\033[?25h')  # Cursor wieder anzeigen
+        print("\n" + Fore.RED + "TERMINATED")
 
-def main():
-    print(Fore.GREEN + "Binärer Farbstrom Generator")
-    print(Fore.LIGHTGREEN_EX + "Drücke STRG+C zum Beenden\n")
-    
-    # Starte den Generator
-    generate_random_binary_stream()
-
-if __name__ == "__main__":
-    main()
+turbo_binary_rain()
